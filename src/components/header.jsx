06a5091930b7119a2d80f6a80232f0e3b6a90a48
Jair montenegro <jairmontenegrof@gmail.com>
2022-10-03 00:00:38 -0300

    add footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,14 +8,13 @@ const Header = () => {
         <header>
 
             <div className="logo">
-                <h1><Link to={navBar[0].link}>LOGO</Link></h1>
+                <h1><Link to={navBar[0].link}>INPSAC</Link></h1>
             </div>
             <nav>
             {navBar.map(link => {
-
-                return (
+                return (    
                         <div key={link.id}>
-                            <Link to={link.link} >{link.text}</Link>
+                            <Link to={link.link}>{link.text}</Link>
                         </div>
                 )
             }
--- src/util/Header-data.js
+++ src/util/Header-data.js
@@ -1,7 +1,8 @@
 const navBar = [
-    {id: "1", text: "Home", link: "/home"},
-    {id: "2", text: "About", link: "/about"},
-    {id: "3", text: "Contact", link: "/contact"}
+  { id: "1", text: "Inicio", link: "/home" },
+  { id: "2", text: "Nosotros", link: "/about" },
+  { id: "3", text: "Blog", link: "/blog" },
+  { id: "4", text: "Contactanos", link: "/contactus" },
 ];
 
-export default navBar
+export default navBar;
