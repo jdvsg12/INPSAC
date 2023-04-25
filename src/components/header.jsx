33bcd8b5cc6fa4099b78861ed212ad6d0089eae7
Jair montenegro <jairmontenegrof@gmail.com>
2023-04-25 13:59:48 -0500

    adding fonts and fixing footer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import navBar from "../util/Header-data";
-
+import logo from "../util/img/inpsac.png";
 
 const Header = () => {
-
-
-    return (
-        <header>
-            <div className="logo">
-                <h1><Link to={navBar[0].link}>INPSAC</Link></h1>
+  return (
+    <header>
+      <div className="logo">
+        <img src={logo} alt="inspac-img"></img>
+        <h1>
+          <Link to={navBar[0].link}>Psicologia y cultura</Link>
+        </h1>
+      </div>
+      <nav>
+        {navBar.map((link) => {
+          return (
+            <div key={link.id}>
+              <Link to={link.link}>{link.text}</Link>
             </div>
-            <nav>
-                {navBar.map(link => {
-
-                    return (
-                        <div key={link.id}>
-                            <Link to={link.link}>{link.text}</Link>
-                        </div>
-                    )
-                }
-                )}
-            </nav>
-        </header>
-    )
-}
+          );
+        })}
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
